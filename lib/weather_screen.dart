d302e70e2adbab1a897acd52b103184c475f3f64
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-03-15 00:30:00 +0100

    Add Card

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -21,23 +22,53 @@ class WeatherScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          // main card
-          const Placeholder(
-            fallbackHeight: 250,
-          ),
-          const SizedBox(height: 20),
-          // weather forecast cards
-          const Placeholder(
-            fallbackHeight: 150,
-          ),
-          const SizedBox(height: 20),
-          // additional information
-          const Placeholder(
-            fallbackHeight: 150,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        '25°C',
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Icon(
+                        Icons.cloud,
+                        size: 54,
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        'Rain',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // weather forecast cards
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+            const SizedBox(height: 20),
+            // additional information
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+          ],
+        ),
       ),
     );
   }
